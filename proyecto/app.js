fdb01f259e3ccb859cedd7b88c7a410d9c051ee1
civky <naruandroid@gmail.com>
2017-09-22 18:42:37 -0300

    Avance login

--- proyecto/app.js
+++ proyecto/app.js
@@ -3,13 +3,18 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+var express 	= require('express');
+var passport 	= require('passport');
+var session 	= require('express-session');
+var bodyParser 	= require('body-parser');
+var routes 		= require('./routes');
+var user 		= require('./routes/user');
+var http 		= require('http');
+var path 		= require('path');
+var env 		= require('dotenv').load();
 
-var app = express();
+var app 		= express();
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -23,6 +28,18 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// Passport
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize()); 
+app.use(passport.session()); // persistent login sessions
+
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
@@ -34,3 +51,20 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+ 
+/*app.get('/', function(req, res) {
+ 
+    res.send('Welcome to Passport with Sequelize');
+ 
+});
+ 
+ 
+app.listen(5000, function(err) {
+ 
+    if (!err)
+        console.log("Site is live");
+    else console.log(err)
+ 
+});*/
+
